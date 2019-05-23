1ab590b6ab7ce29a472d40b444e101f3197d62ec
Wes Kruger <wesleykruger@gmail.com>
2019-05-23 12:00:54 -0500

    added notes to explain steps

--- notice-conversion.py
+++ notice-conversion.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 import base64
 from pathlib import Path
 from bs4 import BeautifulSoup
@@ -9,26 +8,31 @@ css_file = Path("C:/Users/wesleykruger/Documents/BBVA/notice-external-ref/letter
 image_directory = Path("C:/Users/wesleykruger/Documents/BBVA/notice-external-ref/LetterImages")
 b64Dict = {}
 
+# Map out dictionary key/value pairs for encoded images
 for file in os.listdir(image_directory):
     with open(os.path.join(image_directory, file), 'rb') as image_file:
         encoded_string = base64.b64encode(image_file.read())
         b64Dict[file] = encoded_string.decode()
 
 
+# Read out CSS file contents for insertion
 with open(css_file, 'r') as css_file_read:
     css_contents = css_file_read.readlines()
     css_contents_str = '\n\n' + ''.join(css_contents) + '\n\n'
 
 
+# Read through file, instantiate HTML soup
 for file in os.listdir(source_directory):
     with open(os.path.join(Path(source_directory, file)), 'r+', encoding='utf-8') as replace_run:
         soup = BeautifulSoup(replace_run, 'html.parser')
+        # Find CSS link tag and replace it with CSS file contents
         link_tag = soup.link
         if link_tag:
             style_tag = soup.new_tag('style')
             style_tag.string = css_contents_str
             link_tag.replace_with(style_tag)
 
+        # Find all images in the file, overwrite their src with our encoded value pulled from dictionary
         for image in soup.find_all('img'):
             img_src = image['src']
             dictKey = img_src.split('/')[-1]
@@ -37,9 +41,7 @@ for file in os.listdir(source_directory):
             b64_tag['src'] = 'data:image/jpeg;base64,' + DictValue
             image['src'] = 'data:image/jpeg;base64,' + DictValue
 
-        for image in soup.find_all('img'):
-            print(image)
-
+    # Write new file in output directory using modified HTML soup
     with open(os.path.join(Path("C:/Users/wesleykruger/Documents/BBVA/notice-external-ref/fixed-notices", file)), 'w', encoding='utf-8') as replace_write:
         replace_write.write(str(soup))
 
